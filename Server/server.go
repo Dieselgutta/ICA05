c968ebc7b24632131f54c97d35bb2bece4145a8f
tarjeitax <tarjeitax@hotmail.com>
2017-05-15 19:18:35 +0200

    endret server

--- Server/server.go
+++ Server/server.go
@@ -30,14 +30,12 @@ type Weather struct {
 	Main Measurements
 }
 
-//Kjører opp en server på port 8001, og kjører basicHandler funksjonen hver gang man loader "/" på nettsiden, altså index, begynner også en logg med "Listening..."
 func main() {
 	http.HandleFunc("/", basicHandler)
 	http.ListenAndServe(":8001", nil)
 	log.Println("Listening...")
 }
 
-//Henter ut Json data ved hjelp av getData funksjonen, og bruker template pakken for å generere en ny html fil sammen med Json data som returneres til brukeren
 func basicHandler(w http.ResponseWriter, r *http.Request) {
 	json := getData()
 	profile := Weather(*json)
@@ -53,27 +51,22 @@ func basicHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//Dekoder Json data ved hjelp av unmarshal funksjonen, og konverterer på slutten data fra kelvin til celsius før den returneres
 func decode(data []byte) *Weather {
 	dat := new(Weather)
 	err := json.Unmarshal(data, dat)
 	if err != nil{
 		fmt.Println("Error: ", err)
 	}
-	//Konverterer data fra Kelvin til celsius
+
 	dat.Main.Temp = dat.Main.Temp - 273.15
 	dat.Main.Temp_max = dat.Main.Temp_max - 273.15
 	dat.Main.Temp_min = dat.Main.Temp_min - 273.15
 	return dat
 }
 
-//Henter ut data fra OWM ved hjelp av http.Get funksjonen, leser så Body og kjører den igjennom decode før den returneres
 func getData() *Weather {
 	res, _ := http.Get(URL)
 	body, _ := ioutil.ReadAll(res.Body)
 	s := decode(body)
 	return s
 }
-
-//Funksjonen decode og getData kunne ha blitt bygget sammen til en funksjon for å spare plass, men -
-//- vi valgte å dele de opp slik at de er enklere å jobbe videre på senere.
