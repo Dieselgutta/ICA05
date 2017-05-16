30a1348b82fdd1248f1cb75637b99f3db32bec40
RasmusRS <rasmus16@student.uia.no>
2017-05-16 15:07:48 +0200

    flere structs i server og endringer i html

--- Server/server.go
+++ Server/server.go
@@ -1,29 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
-	"net/http"
-	"encoding/json"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"path"
 )
 
 const URL = "http://api.openweathermap.org/data/2.5/weather?id=6453405&appid=2f134dd341ed0970e1bdf7bec5eac617"
 
-type Coordinates struct{
+type Coordinates struct {
 	Lon float64
 	Lat float64
 }
 type Measurements struct {
-	Temp float64
+	Temp     float64
 	Pressure float64
 	Humidity float64
+	Temp_min float64
+	Temp_max float64
+}
+type Sys1 struct {
+	Id      float64
+	Message float64
+	Country string
+	Sunrise float64
+	Sunset  float64
+}
+type Vind struct {
+	Speed float64
+	Deg   float64
 }
 type Weather struct {
 	Coord Coordinates
-	Main Measurements
+	Main  Measurements
+	Sys   Sys1
+	Wind  Vind
 }
 
 func main() {
@@ -50,7 +65,7 @@ func basicHandler(w http.ResponseWriter, r *http.Request) {
 func decode(data []byte) *Weather {
 	dat := new(Weather)
 	err := json.Unmarshal(data, dat)
-	if err != nil{
+	if err != nil {
 		fmt.Println("Error: ", err)
 	}
 
